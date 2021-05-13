131f889197654be6697a9d05d7aa90eb081c712f
amrut301 <malaji.amrut@gmail.com>
2021-05-13 18:47:14 +0530

    updating the Rule Engine

--- src/main/java/com/sps/rule/RuleEngine.java
+++ src/main/java/com/sps/rule/RuleEngine.java
@@ -2,7 +2,6 @@ package com.sps.rule;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.stream.Collectors;
 
 import com.rule.sps.RateChange;
 
@@ -21,22 +20,22 @@ public class RuleEngine<R, A> {
 
 	public RateChange executeRule(RateChange rate) {
 
-		return rules.stream().filter(rule -> rule.matches(rate)).map(rule -> (RateChange) rule.process(rate))
-				.findFirst().orElseThrow(() -> new RuntimeException("No matching rule found"));
+		return (RateChange) rules.stream().filter(rule -> rule.matches(rate)).map(rule -> (RateChange) rule.process(rate)).findAny().get();
+//				.findFirst().orElseThrow(() -> new RuntimeException("No matching rule found"));
 
 	}
 
-	public List<RateChange> executeRules(RateChange rate) {
-		/**
-		 * this is returning list, we can change it to return one item and apply rules
-		 * in sequential order
-		 */
-		List<RateChange> rateAfterRule = rules.stream().filter(rule -> rule.matches(rate))
-				.map(rule -> (RateChange) rule.process(rate)).collect(Collectors.toList());
-		return rateAfterRule;
-//		return executeRule(executeRule(rate));
-
-	}
+//	public List<RateChange> executeRules(RateChange rate) {
+//		/**
+//		 * this is returning list, we can change it to return one item and apply rules
+//		 * in sequential order
+//		 */
+//		List<RateChange> rateAfterRule = rules.stream().filter(rule -> rule.matches(rate))
+//				.map(rule -> (RateChange) rule.process(rate)).collect(Collectors.toList());
+//		return rateAfterRule;
+////		return executeRule(executeRule(rate));
+//
+//	}
 
 	public RuleEngine<R, A> configureRule(AnomalyRule<RateChange, RateChange> rule) {
 		rules.add(rule);
